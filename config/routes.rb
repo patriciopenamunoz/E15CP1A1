f75a255262bfed50af6203a9cae5b4abc5361005
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-09-23 23:33:06 +0000

    Create methods and routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :histories
-
+  get 'users/sign_up', to: 'users#new'
+  post 'users', to: 'users#create'
   root "histories#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
